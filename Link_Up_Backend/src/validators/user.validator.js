9675c2981d4f57530c5bf175536ddc36a4e63c0d
Gaurav Saha <gauravsaha024@gmail.com>
2026-04-13 22:54:13 +0530

    Phase 5: to be continued...

--- Link_Up_Backend/src/validators/user.validator.js
+++ Link_Up_Backend/src/validators/user.validator.js
@@ -1,26 +1,27 @@
-const {anything} = require("zod")
+const { z } = require("zod")
 
-const userprofileSchema = anything.object({
-    name: anything
+const updateProfileSchema = z.object({
+    name: z
         .string()
         .max(50, 'Name must be under 50 characters')
-        .min(5 , 'Atleast 5 charactes')
+        .min(5, 'Atleast 5 characters')
         .optional(),
-    bio: anything
+    bio: z
         .string()
         .max(200, "Bio must be under 200 characters")
         .min(10, "write something")
         .optional(),
-        username: anything
-        .max(30, "Maximun 30 characters")
+    username: z
+        .string()
+        .max(30, "Maximum 30 characters")
         .regex(/^[a-zA-Z0-9_]+$/, 'Username can only contain letters, numbers, and underscores')
         .optional(),
 });
 
-const useravatar = anything.object({
-    avatar: anything
-        .string({required_error: "Avatar url is required"})
-        .url('Avatar must have url')
+const updateAvatarSchema = z.object({
+    avatar: z
+        .string({ required_error: "Avatar url is required" })
+        .url('Avatar must have a valid url')
 })
 
-module.exports= { userprofile }
+module.exports = { updateProfileSchema, updateAvatarSchema }
