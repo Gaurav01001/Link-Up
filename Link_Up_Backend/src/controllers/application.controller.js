cf315daa7803a01a793fc2b6fa14ea367b3fab33
Gaurav Saha <gauravsaha024@gmail.com>
2026-05-06 22:33:25 +0530

    Phase 6: to be continued...

--- Link_Up_Backend/src/controllers/application.controller.js
+++ Link_Up_Backend/src/controllers/application.controller.js
@@ -1,5 +1,6 @@
-const { applyToRole } = require('../services/application.service');
-
+const { success } = require('zod');
+const { message } = require('../config/prisma');
+const { applyToRole, getApplicationsForRole, updateApplicationStatus: updateApplicationStatusService } = require('../services/application.service');
 
 async function applyRole(req, res) {
   try {
@@ -9,18 +10,62 @@ async function applyRole(req, res) {
     const application = await applyToRole(roleId, userId);
 
     res.status(201).json({
+      success: true,
       message: 'Application submitted successfully',
-      application,
+      data: application,
     });
-  } 
-    catch (error) {
+
+  } catch (error) {
     res.status(400).json({
-        success: false,
-        message: error.message
+      success: false,
+      message: error.message
     });
+  }
 }
+
+async function updateApplication(req, res) {
+  try {
+    const applicationId = req.params.id;
+    const userId = req.user.id;
+    const { status } = req.body;
+
+    const updated = await updateApplicationStatusService(applicationId, userId, status);
+
+    res.status(200).json({
+      success: true,
+      data: updated,
+    });
+
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
+}
+
+async function getApplications(req, res) {
+  try {
+    const roleId = req.params.id;
+    const userId = req.user.id;
+
+    const apps = await getApplicationsForRole(roleId, userId);
+
+    res.status(200).json({
+      success: true,
+      data: apps,
+    });
+
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
 }
 
 module.exports = {
   applyRole,
+  getApplications,
+  updateApplication,
 };
