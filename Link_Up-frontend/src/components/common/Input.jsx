df326488af500288d0ae4a11be4e4ec7984b0d20
Gaurav Saha <gauravsaha024@gmail.com>
2026-06-02 00:11:54 +0530

    step 13 done.

--- Link_Up-frontend/src/components/common/Input.jsx
+++ Link_Up-frontend/src/components/common/Input.jsx
@@ -1,1 +1,34 @@
+import React from "react";
 
+const Input = ({
+  label,
+  type = "text",
+  value,
+  onChange,
+  placeholder,
+  error,
+  icon,
+  ...props
+}) => {
+  return (
+    <div className="input-group">
+      {label && <label>{label}</label>}
+
+      <div className="input-wrapper">
+        {icon && <span>{icon}</span>}
+
+        <input
+          type={type}
+          value={value}
+          onChange={onChange}
+          placeholder={placeholder}
+          {...props}
+        />
+      </div>
+
+      {error && <p>{error}</p>}
+    </div>
+  );
+};
+
+export default Input;
